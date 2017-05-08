5e582a06149dc07ad811a7efe42a4800a73c91e3
Ben Tristem <github@bentristem.com>
2017-05-08 22:21:45 +0100

    Donkey (for want of better name) spawns character model

---
+++ Assets/_Characters/Character.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RPG.Characters
+{
+	[CreateAssetMenu(menuName = ("RPG/Character"))]
+	public class Character : ScriptableObject {
+		
+		public GameObject animatedModel;
+
+	}
+}
---
+++ Assets/_Characters/Donkey.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Characters
+{
+	public class Donkey : MonoBehaviour {
+
+		[SerializeField] Character characterConfig;
+
+		// Use this for initialization
+		void Start () {
+			var characterModel = characterConfig.animatedModel;
+			Instantiate (characterModel, gameObject.transform);
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+	}
+}
