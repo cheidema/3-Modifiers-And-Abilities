a7cbbc057644ca80827c6e3928c9722230dbb89f
Ben Tristem <github@bentristem.com>
2017-05-09 15:04:51 +0100

    I don’t know how to describe what we just did

--- Assets/_Characters/Character.cs
+++ Assets/_Characters/Character.cs
@@ -1,55 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.AI;
+﻿using UnityEngine;
+using UnityEngine.Assertions;
 
-namespace RPG.Characters
-{
-	[ExecuteInEditMode]
-	[SelectionBase]
-	public class Character : MonoBehaviour {
+public class Character {
 
-		[SerializeField] CharacterConfig characterConfig;
+	public void Move (Vector2 relativelyVelocity)
+	{
+		Assert.IsTrue (relativelyVelocity.magnitude <= 1f); // Like gamepad
 
-		// Use this for initialization
-		void Start () {
-			RebuildChildren ();
-			if (Application.isPlaying)
-			{
-				gameObject.AddComponent<NavMeshAgent> ();
-			}
-		}
 
-		void DestroyChildren ()
-		{
-			foreach (Transform child in transform)
-			{
-				DestroyImmediate (child.gameObject);
-			}
-		}
-
-		void RebuildChildren ()
-		{
-			if (characterConfig == null)
-			{
-				// TODO consider making show only once
-				Debug.LogWarning ('"' + gameObject.name + '"' + " has no character config");
-				return;
-			}
-			DestroyChildren ();
-			var model = Instantiate (characterConfig.animatedModel, gameObject.transform);
-			if (!Application.isPlaying) // TODO move to animation shim
-			{
-				model.transform.Rotate (Vector3.up, 180f);
-			}
-		}
-		
-		// Update is called once per frame
-		void Update () {
-			if (!Application.isPlaying)
-			{
-				RebuildChildren ();
-			}
-		}
 	}
 }
---
+++ Assets/_Characters/CharacterFactory.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Characters
+{
+	[ExecuteInEditMode]
+	[SelectionBase]
+	public class CharacterFactory : MonoBehaviour {
+
+		[SerializeField] CharacterConfig characterConfig;
+		[SerializeField] Waypoints wayPoints;
+
+		// Use this for initialization
+		void Start () {
+			RebuildChildren ();
+			if (Application.isPlaying)
+			{
+				gameObject.AddComponent<NavMeshAgent> ();
+			}
+		}
+
+		void DestroyChildren ()
+		{
+			foreach (Transform child in transform)
+			{
+				DestroyImmediate (child.gameObject);
+			}
+		}
+
+		void RebuildChildren ()
+		{
+			if (characterConfig == null)
+			{
+				// TODO consider making show only once
+				Debug.LogWarning ('"' + gameObject.name + '"' + " has no character config");
+				return;
+			}
+			DestroyChildren ();
+			var model = Instantiate (characterConfig.animatedModel, gameObject.transform);
+			if (!Application.isPlaying) // TODO move to animation shim
+			{
+				model.transform.Rotate (Vector3.up, 180f);
+			}
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			if (!Application.isPlaying)
+			{
+				RebuildChildren ();
+			}
+		}
+	}
+}
