429966507aae16c3bf6be8b8cd44e4a5d7d822ee
Ben Tristem <github@bentristem.com>
2017-05-09 13:56:08 +0100

    Patrol path setup

---
+++ Assets/Path.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnDrawGizmos()
+	{
+		Vector3 firstPosition = transform.GetChild (0).position;
+		Vector3 previousPosition = firstPosition;
+
+		foreach (Transform waypoint in transform)
+		{
+			Gizmos.DrawSphere (waypoint.position, .2f);
+			Gizmos.DrawLine (previousPosition, waypoint.position);
+			previousPosition = waypoint.position;
+		}
+		Gizmos.DrawLine (previousPosition, firstPosition);
+	}
+
+}
