a477f35314f8e39e4b151e741898e3c4a54d09f4
Ben Tristem <github@bentristem.com>
2017-05-09 11:51:46 +0100

    Previews model in editor, and [SelectionBase] annotation used

--- Assets/_Characters/Donkey.cs
+++ Assets/_Characters/Donkey.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 namespace RPG.Characters
 {
+	[ExecuteInEditMode]
+	[SelectionBase]
 	public class Donkey : MonoBehaviour {
 
 		[SerializeField] Character characterConfig;
 
 		// Use this for initialization
 		void Start () {
-			var characterModel = characterConfig.animatedModel;
-			Instantiate (characterModel, gameObject.transform);
+			DestroyChildren ();
+			Instantiate (characterConfig.animatedModel, gameObject.transform);
+
 		}
+
+		void DestroyChildren ()
+		{
+			foreach (Transform child in transform)
+			{
+				DestroyImmediate (child.gameObject);
+			}
+		}
+
 		
 		// Update is called once per frame
 		void Update () {
-			
+			if (!Application.isPlaying)
+			{
+				DestroyChildren ();
+				Instantiate (characterConfig.animatedModel, gameObject.transform);
+			}
 		}
 	}
 }
