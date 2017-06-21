079144fcc26604d97696a0bb3ce1d2798536b481
Ben Tristem <github@bentristem.com>
2017-06-21 09:15:24 +0100

    Re-propduction conditions for JitterBug

---
+++ Assets/_Core/Delay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delay : MonoBehaviour {
+    
+	// Designed to reduce framerate, value picked for Ben's MacBook
+	void Update () {
+        for (int i = 0; i < 500; i++)
+        {
+            Debug.Log(i);
+        }
+	}
+}
