4df981e0beb1546581e0492838627dd20b78abcb
Ben Tristem <github@bentristem.com>
2017-06-22 14:48:37 +0100

    Self heal special ability
    + Abilities map to number keys per array index
    + AOE on key 1
    + Self heal currently on key 2

---
+++ Assets/_Characters/Special Abilities/Self Heal/SelfHealBehaviour.cs
@@ -0,0 +1,37 @@
+﻿﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Characters
+{
+    public class SelfHealBehaviour : MonoBehaviour, ISpecialAbility
+    {
+        SelfHealConfig config;
+
+        Player player;
+
+        public void SetConfig(SelfHealConfig configToSet)
+        {
+            this.config = configToSet;
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            print("Self Heal behaviour attached to " + gameObject.name);
+            player = FindObjectOfType<Player>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void Use(AbilityUseParams useParams)
+        {
+            print("Self heal used by: " + gameObject.name);
+            player.TakeDamage(-config.GetExtraHealth());
+        }
+    }
+}
